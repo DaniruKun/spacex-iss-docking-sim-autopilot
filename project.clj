79de633d36f7dc423751eef586d627ed012d9ebd
dpetrovs <dpetrovs@evolutiongaming.com>
2020-05-24 22:45:38 +0300

    Add CI, update version and description

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject iss-sim-auto-docking "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject iss-sim-auto-docking "0.1.1"
+  :description "This app does automatic docking in the ISS SpaceX simulator."
+  :url "https://github.com/DaniruKun/spacex-iss-docking-sim-autopilot"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [etaoin "0.3.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
-  :main ^:skip-aot iss-sim-auto-docking.core
+  :main iss-sim-auto-docking.core
   :target-path "target/%s"
   :jar-name "iss-sim-auto-docking.jar"
   :uberjar-name "iss-sim-auto-docking-standalone.jar"
