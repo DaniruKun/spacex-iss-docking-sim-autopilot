afc80f60232c0abce5c9d8558a306f7de9d68cf7
dpetrovs <dpetrovs@evolutiongaming.com>
2020-06-07 12:39:36 +0300

    Implement abstract metric parser

--- test/iss_sim_auto_docking/telemetry_test.clj
+++ test/iss_sim_auto_docking/telemetry_test.clj
@@ -1,2 +1,12 @@
-(ns iss-sim-auto-docking.dragon
-  (:require [clojure.test :refer :all]))
+(ns iss-sim-auto-docking.telemetry_test
+  (:require [clojure.test :refer :all]
+            [iss-sim-auto-docking.telemetry :refer :all]))
+
+(deftest parse-metric-test
+  "Test metric parser with different inputs"
+  (testing "Test rate parser"
+    (is (= (float 1.3) (parse-metric "1.3 °/s" :roll-rate))))
+  (testing "Test range parser"
+    (is (= (float -10.2) (parse-metric "-10.2 m" :range))))
+  (testing "Test velocity parser"
+    (is (= (float 1.4) (parse-metric "1.4 m/s" :vel)))))
